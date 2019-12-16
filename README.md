c8acff03e65cdcb28661540ee841ad24c93d79ea
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2019-12-16 04:20:51 +0000

    Setup environment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # advent-of-code-19
+
+My solutions to [Advent of Code 2019](https://adventofcode.com/2019) in [Crystal](https://crystal-lang.org/)
---
+++ src/advent-of-code-19.cr
@@ -0,0 +1,39 @@
+require "./**"
+require "option_parser"
+
+macro print_method(method)
+  print {{ method[4..method.size].gsub(/_/, ".") }}, ": ", AdventOfCode.{{ method.id }}, "\n"
+end
+
+
+all = false
+OptionParser.parse do |parser|
+  parser.banner = "Usage: advent-of-code-19 [arguments]"
+
+  parser.on("-a", "--all", "Runs all solutions") do
+    {% for method, _index in AdventOfCode.methods.map(&.name.stringify).select{ |name| name =~ /run_(\d+_\d)/ } %}
+      print_method({{method}})
+    {% end %}
+  end
+
+  parser.on("-r NAME", "--run=NAME", "Specifies the code to run. Supports regex") do |parse|
+    next if all
+    {% for method, _index in AdventOfCode.methods.map(&.name.stringify).select{ |name| name =~ /run_(\d+_\d)/ } %}
+      if {{ method[4..method.size].gsub(/_/, ".") }} =~ Regex.new(parse)
+        print_method({{method}})
+      end
+    {% end %}
+  end
+
+  parser.on("-h", "--help", "Show this help") { puts parser }
+  parser.invalid_option do |invalid_option|
+    STDERR.puts "ERROR: #{invalid_option} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+  parser.missing_option do |missing_option|
+    STDERR.puts "ERROR: #{missing_option} missing option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
