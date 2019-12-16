2f655142d04a5b4537edd7a70e7c6c6615a3f064
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2019-12-16 04:27:12 +0000

    Reformat spec files

--- spec/1.1_spec.cr
+++ spec/1.1_spec.cr
@@ -1,15 +1,13 @@
 require "./spec_helper"
 
 describe AdventOfCode do
-  describe "1" do
-    describe "1.1" do
-      describe "mass_of_fuel" do
-        it "passes example" do
-          AdventOfCode.mass_of_fuel(12).should eq 2
-          AdventOfCode.mass_of_fuel(14).should eq 2
-          AdventOfCode.mass_of_fuel(1969).should eq 654
-          AdventOfCode.mass_of_fuel(100756).should eq 33583
-        end
+  describe "1.1" do
+    describe "mass_of_fuel" do
+      it "passes example" do
+        AdventOfCode.mass_of_fuel(12).should eq 2
+        AdventOfCode.mass_of_fuel(14).should eq 2
+        AdventOfCode.mass_of_fuel(1969).should eq 654
+        AdventOfCode.mass_of_fuel(100756).should eq 33583
       end
     end
   end
