afb38853e9f4b0fdb0acdaef5d61bb52341ff093
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2019-12-16 06:09:25 +0000

    Add 2.2

---
+++ spec/2.2_spec.cr
@@ -0,0 +1,11 @@
+require "./spec_helper"
+
+describe AdventOfCode do
+  describe "2.2" do
+    describe "nouns_and_verbs" do
+      it "passes example" do
+        AdventOfCode.nouns_and_verbs(File.read("#{__DIR__}/../src/input/2.txt"), 0..99, 0..99, 3058646).should eq(1202)
+      end
+    end
+  end
+end
---
+++ src/2.2.cr
@@ -0,0 +1,19 @@
+module AdventOfCode
+  extend self
+
+  def nouns_and_verbs(string, range_one, range_two, expectation)
+    range_one.each do |noun|
+      range_two.each do |verb|
+        program = AdventOfCode::Intcode.new(string)
+        program.state[1] = noun
+        program.state[2] = verb
+        return 100 * noun + verb if program.run.state[0] == expectation
+      end
+    end
+    false
+  end
+
+  def run_2_2
+    nouns_and_verbs(File.read("#{__DIR__}/input/2.txt"), 0..99, 0..99, 19690720)
+  end
+end
