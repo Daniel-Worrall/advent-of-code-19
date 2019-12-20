a9e0a115cccfa04539c01dea43d9e6c821ce32fa
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2019-12-20 10:41:57 +0000

    Add 5.1

---
+++ spec/5.1_spec.cr
@@ -0,0 +1,36 @@
+require "./spec_helper"
+
+describe AdventOfCode do
+  describe "5.1" do
+    describe AdventOfCode::Intcode do
+      it "passes example" do
+        AdventOfCode::Intcode.new("1,0,0,0,99").run.state.join(",").should eq("2,0,0,0,99")
+        AdventOfCode::Intcode.new("2,3,0,3,99").run.state.join(",").should eq("2,3,0,6,99")
+        AdventOfCode::Intcode.new("2,4,4,5,99,0").run.state.join(",").should eq("2,4,4,5,99,9801")
+        AdventOfCode::Intcode.new("1,1,1,4,99,5,6,0,99").run.state.join(",").should eq("30,1,1,4,2,5,6,0,99")
+
+        AdventOfCode::Intcode.new("1,9,10,3,2,3,11,0,99,30,40,50").run.state.join(",").should eq("3500,9,10,70,2,3,11,0,99,30,40,50")
+      end
+
+      it "stores the input" do
+        intcode = AdventOfCode::Intcode.new("3,0,4,0,99")
+        intcode.run(50)
+        intcode.input.should eq 50
+      end
+
+      it "inputs" do
+        intcode = AdventOfCode::Intcode.new("3,0,99")
+        intcode.run(50)
+        intcode.state[0].should eq 50
+      end
+
+      it "outputs" do
+        AdventOfCode::Intcode.new("4,0,99").run(0)[-1].should eq 4
+      end
+
+      it "inputs and outputs" do
+        AdventOfCode::Intcode.new("3,0,4,0,99").run(50)[-1].should eq 50
+      end
+    end
+  end
+end
---
+++ src/crystal/5.1.cr
@@ -0,0 +1,49 @@
+module AdventOfCode
+  class Intcode
+    property input : Int32 = 0
+    property output = [] of Int32
+
+    def run(@input)
+      counter = 0
+      while state[instruction_pointer] != 99
+        step
+        counter += 1
+        break if counter > 1000
+      end
+      output
+    end
+
+    def step
+      instruction = state[instruction_pointer]
+      modes = (instruction // 100).to_s.chars.map(&.to_i)
+      case opcode = instruction % 100
+      when 1
+        state[state[instruction_pointer + 3]] = parameter(1, modes) + parameter(2, modes)
+        @instruction_pointer += 4
+      when 2
+        state[state[instruction_pointer + 3]] = parameter(1, modes) * parameter(2, modes)
+        @instruction_pointer += 4
+      when 3
+        state[state[instruction_pointer + 1]] = input
+        @instruction_pointer += 2
+      when 4
+        output << state[state[instruction_pointer + 1]]
+        @instruction_pointer += 2
+      when 99
+      else
+        raise "Unknown opcode #{opcode}"
+      end
+      opcode
+    end
+
+    private def parameter(number, modes)
+      state[modes[-number]? == 1 ? instruction_pointer + number : state[instruction_pointer + number]]
+    end
+  end
+
+  extend self
+
+  def run_5_1
+    AdventOfCode::Intcode.new(File.read("#{__DIR__}/input/5.txt")).run(1)[-1]
+  end
+end
